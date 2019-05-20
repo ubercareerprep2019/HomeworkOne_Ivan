732973ac22fe43254529b1e4e978b05f91541ddb
Ivan Esmeral <ivanes@seas.upenn.edu>
2019-05-19 23:28:30 -0700

    fixing up setup

---
+++ src/com/ArraysAndStrings/ArraysAndStrings.java
@@ -0,0 +1,8 @@
+package com.ArraysAndStrings;
+
+public class ArraysAndStrings {
+
+    public boolean isStringPermutation(String s1, String s2) {
+
+    }
+}
---
+++ tests/com/ArraysAndStrings/ArraysAndStringsTest.java
@@ -0,0 +1,13 @@
+package com.ArraysAndStrings;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArraysAndStringsTest {
+
+    @Test
+    void isStringPermutation() {
+
+    }
+}
