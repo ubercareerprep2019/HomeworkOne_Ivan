f52b14b387a76e5a5380c9be431d6804bc10b0fd
Ivan Esmeral <ivanes@seas.upenn.edu>
2019-05-23 16:48:45 -0700

    permutation problem with unit test

--- src/com/ArraysAndStrings/ArraysAndStrings.java
+++ src/com/ArraysAndStrings/ArraysAndStrings.java
@@ -1,8 +1,52 @@
 package com.ArraysAndStrings;
 
+import java.util.HashMap;
+
 public class ArraysAndStrings {
 
-    public boolean isStringPermutation(String s1, String s2) {
+    // ideas:
+    // error check and edge case check
+    // would want everything to be same case
 
+    // iterate through each string
+    // add each character to a map that has the char as the character and the frequency count as the value
+    // have map for a single word and then use other word as a test checker, decreementing the value of the fre q
+    // iterate trough  map and if u get all instances of zeroes then return true
+
+    // runtime O(n)
+    public static boolean isStringPermutation(String s1, String s2) {
+        if (s1 == null || s2 == null) {
+            throw new IllegalArgumentException("Illegal input string");
+        }
+        if (s1.length() != s2.length()) {
+            return false;
+        }
+        String updatedS1 = s1.toLowerCase();
+        String updatedS2 = s2.toLowerCase();
+        HashMap<Character, Integer> permutationChecker =  new HashMap<>();
+        for (int i = 0; i < updatedS1.length(); i++) {
+            //case where character is already in map and needs to be incremented
+            if (permutationChecker.containsKey(updatedS1.charAt(i))) {
+                int incrementedCharFrequency = permutationChecker.get(updatedS1.charAt(i)) + 1;
+                permutationChecker.replace(updatedS1.charAt(i), incrementedCharFrequency);
+            } else {
+
+                // case where chracter is being seen for first time
+                permutationChecker.put(updatedS1.charAt(i), 1);
+            }
+        }
+        // using second string to test against string 1 -  at end of loop should have all zeroes if permutation
+        for (int i = 0; i < updatedS2.length(); i++) {
+            if (permutationChecker.containsKey(updatedS2.charAt(i)))  {
+                int decrementedCharFrequency = permutationChecker.get(updatedS1.charAt(i)) - 1;
+                permutationChecker.replace(updatedS1.charAt(i), decrementedCharFrequency);
+            }
+        }
+        for (int i = 0; i < updatedS1.length(); i++) {
+            if (permutationChecker.get(updatedS1.charAt(i)) != 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
--- tests/com/ArraysAndStrings/ArraysAndStringsTest.java
+++ tests/com/ArraysAndStrings/ArraysAndStringsTest.java
@@ -1,13 +1,36 @@
 package com.ArraysAndStrings;
 
 import org.junit.jupiter.api.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ArraysAndStringsTest {
 
     @Test
-    void isStringPermutation() {
+    void isStringPermutationFalseCase() {
+        String s1 = "bbbbbb";
+        String s2 = "aaaaaa";
+        assertFalse(ArraysAndStrings.isStringPermutation(s1, s2));
+    }
+    @Test
+    void isStringPermutationIdenticalCase() {
+        String s1 = "bbb";
+        String s2 = "aaa";
+        assertFalse(ArraysAndStrings.isStringPermutation(s1, s2));
+    }
+    @Test
+    void isStringPermutationTrueCase() {
+        String s1 = "ivanesmeral";
+        String s2 = "esmeralivan";
+        assertTrue(ArraysAndStrings.isStringPermutation(s1, s2));
+    }
 
+    @Test
+    void isStringPermutationEdgeCase() {
+        String s1 = "ivanESMERAL";
+        String s2 = "esmeralIVAN";
+        assertTrue(ArraysAndStrings.isStringPermutation(s1, s2));
     }
 }
